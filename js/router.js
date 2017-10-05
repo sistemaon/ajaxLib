e0477b0cd31ffbd79bae704bf065de9f0b08ed26
Suissa <jnascimento@gmail.com>
2017-10-05 06:53:17 -0300

    code3 style

--- js/router.js
+++ js/router.js
@@ -15,96 +15,92 @@ const views = [
 ]
 
 const getViewByType = ( type ) => ( view ) => {
-  let o = document.createElement("object")
+  let o = document.createElement( 'object' )
   o.data = view.path
   o.type = type
   return o
 }
 
-const getViewHTML = getViewByType('text/html')
+const getViewHTML = getViewByType( 'text/html' )
 
-const toViews = (views) => ( obj, view, i ) => {
-  console.log('------------------------------------');
-  console.log(obj);
-  console.log('------------------------------------');
+const toViews = ( views ) => ( obj, view, i ) => {
+  console.log( '------------------------------------' )
+  console.log( obj )
+  console.log( '------------------------------------' )
   const o = {
-    [views[i].route]: view
+    [ views[ i ].route ]: view
   }
   return Object.assign( obj, o )
 }
 
-const myViews = views.map(getViewHTML).reduce(toViews(views), {})
+const myViews = views.map( getViewHTML ).reduce( toViews( views ), {} )
 
-console.log('------------------------------------');
-console.log('myViews: ', myViews);
-console.log('------------------------------------');
+console.log( '------------------------------------' )
+console.log( 'myViews: ', myViews )
+console.log( '------------------------------------' )
 
 // myViews.forEach( view => {
-//   const div = document.createElement("div")
-//   div.appendChild(view);
-//   console.log('------------------------------------');
-//   console.log('div', div);
-//   console.log('------------------------------------');
-//   document.body.appendChild(div);
-// })
+//   const div = document.createElement( "div" )
+//   div.appendChild( view )
+//   console.log( '------------------------------------' )
+//   console.log( 'div', div )
+//   console.log( '------------------------------------' )
+//   document.body.appendChild( div )
+// } )
 
+const getELementId = ( id ) => document.getElementById( id )
+const qrSelectorAll = ( id ) => document.querySelectorAll( id )
 
-const getELementId = (id) => document.getElementById(id);
-const qrSelectorAll = (id) => document.querySelectorAll(id);
+const getElementValue = ( id ) => getELementId( id ).value
 
-const getElementValue = (id) => getELementId(id).value;
-
-const addEventTo = (el, ev, run) => el.addEventListener(ev, run);
-
+const addEventTo = ( el, ev, run ) => el.addEventListener( ev, run )
 
 const hide = ( element ) => element.style.display = 'none'
 
-const changeURL = ( url ) => history.pushState(null, null, url);
+const changeURL = ( url ) => history.pushState( null, null, url )
 
 const changeView = ( viewOld, viewNew ) => {
-  getELementId(viewOld).classList.add('hide')
-  getELementId(viewNew).classList.remove('hide')
+  getELementId( viewOld ).classList.add( 'hide' )
+  getELementId( viewNew ).classList.remove( 'hide' )
 }
-// const btn = document.getElementById('changeRoute')
-
+// const btn = document.getElementById( 'changeRoute' )
 
 const changeViewByClass = ( viewOld, viewNew ) => {
-  getELementId(viewOld).classList.add('hide')
-  getELementId(viewNew).classList.remove('hide')
+  getELementId( viewOld ).classList.add( 'hide' )
+  getELementId( viewNew ).classList.remove( 'hide' )
 }
 
-const hideViews = ( views ) => 
-  views.forEach(function(element) {
-    element.classList.add('hide');
-  }, this);
+const hideViews = ( views ) =>
+views.forEach( function ( element ) {
+  element.classList.add( 'hide' )
+}, this )
 
-const changeRoute = (evt) => {
+const changeRoute = ( evt ) => {
   const url = evt.toElement.dataset.url
-  // console.log('------------------------------------');
-  // console.log(evt.toElement.dataset.url);
-  // console.log(url);
-  // console.log('------------------------------------');
-  // hideViews(qrSelectorAll('.view'))
-  changeURL(url + '.html')
-  // changeViewByClass('view', 'view0')
-  
-  getELementId(VIEW_WRAPPER_ID).innerHTML = ''
-  getELementId(VIEW_WRAPPER_ID).appendChild(myViews[url])
-  
-  // qrSelectorAll('.view').forEach(function(element) {
-  //   element.classList.add('hide');
-  // }, this);
+  // console.log( '------------------------------------' )
+  // console.log( evt.toElement.dataset.url )
+  // console.log( url )
+  // console.log( '------------------------------------' )
+  // hideViews( qrSelectorAll( '.view' ) )
+  changeURL( url + '.html' )
+  // changeViewByClass( 'view', 'view0' )
 
-  // getELementId(url).classList.remove('hide');
-  
+  getELementId( VIEW_WRAPPER_ID ).innerHTML = ''
+  getELementId( VIEW_WRAPPER_ID ).appendChild( myViews[ url ] )
+
+  // qrSelectorAll( '.view' ).forEach( function( element ) {
+  //   element.classList.add( 'hide' )
+  // }, this )
+
+  // getELementId( url ).classList.remove( 'hide' )
+
 }
-// const buttonChangeRoute = addEventTo(getELementId('changeRoute'), 'click', changeRoute)
+// const buttonChangeRoute = addEventTo( getELementId( 'changeRoute' ), 'click', changeRoute )
 // }
 
+const addEventsTo = ( els, ev, run ) =>
+els.forEach( function ( element ) {
+  element.addEventListener( ev, run, false )
+}, this )
 
-const addEventsTo = (els, ev, run) => 
-  els.forEach(function(element) {
-    element.addEventListener(ev,run,false);
-  }, this);
-
-const menu = addEventsTo(qrSelectorAll('.linkPage'), 'click', changeRoute)
+const menu = addEventsTo( qrSelectorAll( '.linkPage' ), 'click', changeRoute )
