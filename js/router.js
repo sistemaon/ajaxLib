d038e407bbc1dab0936c46c8579c1e15b02fd18e
Suissa <jnascimento@gmail.com>
2017-10-05 06:49:11 -0300

    Nosso pequeno router

--- js/router.js
+++ js/router.js
@@ -1,3 +1,53 @@
+const VIEW_WRAPPER_ID = 'view'
+const views = [
+  {
+    path: 'page1.html',
+    route: 'page1'
+  },
+  {
+    path: 'page2.html',
+    route: 'page2'
+  },
+  {
+    path: 'page3.html',
+    route: 'page3'
+  }
+]
+
+const getViewByType = ( type ) => ( view ) => {
+  let o = document.createElement("object")
+  o.data = view.path
+  o.type = type
+  return o
+}
+
+const getViewHTML = getViewByType('text/html')
+
+const toViews = (views) => ( obj, view, i ) => {
+  console.log('------------------------------------');
+  console.log(obj);
+  console.log('------------------------------------');
+  const o = {
+    [views[i].route]: view
+  }
+  return Object.assign( obj, o )
+}
+
+const myViews = views.map(getViewHTML).reduce(toViews(views), {})
+
+console.log('------------------------------------');
+console.log('myViews: ', myViews);
+console.log('------------------------------------');
+
+// myViews.forEach( view => {
+//   const div = document.createElement("div")
+//   div.appendChild(view);
+//   console.log('------------------------------------');
+//   console.log('div', div);
+//   console.log('------------------------------------');
+//   document.body.appendChild(div);
+// })
+
 
 const getELementId = (id) => document.getElementById(id);
 const qrSelectorAll = (id) => document.querySelectorAll(id);
@@ -34,16 +84,18 @@ const changeRoute = (evt) => {
   // console.log(evt.toElement.dataset.url);
   // console.log(url);
   // console.log('------------------------------------');
-  hideViews(qrSelectorAll('.view'))
+  // hideViews(qrSelectorAll('.view'))
   changeURL(url + '.html')
   // changeViewByClass('view', 'view0')
   
+  getELementId(VIEW_WRAPPER_ID).innerHTML = ''
+  getELementId(VIEW_WRAPPER_ID).appendChild(myViews[url])
   
   // qrSelectorAll('.view').forEach(function(element) {
   //   element.classList.add('hide');
   // }, this);
 
-  getELementId(url).classList.remove('hide');
+  // getELementId(url).classList.remove('hide');
   
 }
 // const buttonChangeRoute = addEventTo(getELementId('changeRoute'), 'click', changeRoute)
